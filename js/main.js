2625485323d78ce3cf4e4dd3ec8e52963689fba6
Linux Dude <51220763+imlinuxdude@users.noreply.github.com>
2019-06-01 09:11:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@ var nav_link = document.getElementsByClassName("nav-link");
 for(i = 0; i < nav_link.length; i++){
     if(nav_link[i].href==window.location.href){
         nav_link[i].classList.add("active-link");
+        
+        if(i!=0){
+            nav_link[0].classList.remove("active_link");
+        }
     }
-    if(i>0){
-        nav_link[0].classList.remove("active_link");
-    }
+    
 }
 var counterId = 1;
 function counter(){
