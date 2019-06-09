415299b1a3ddf56b948e13aa172d38f0e88adb0d
Linux Dude <51220763+imlinuxdude@users.noreply.github.com>
2019-06-09 18:30:03 +0530

    Update blog-main.js

--- Blog/js/blog-main.js
+++ Blog/js/blog-main.js
@@ -29,7 +29,7 @@ postDataR.onload = function(){
         var h4 = document.createElement("h4");
         var p = document.createElement("p");
         col.classList.add("col");
-        a.href = "#";
+        a.href = "post.html";
         image.classList.add("image");
         h4.classList.add("post-title");
         p.classList.add("post-data");
@@ -58,4 +58,42 @@ function uploadData(){
     }
 }
 postDataR.send();
+}
+
+
+//post 
+
+var post = document.getElementsByClassName("post-content")[0];
+
+postDataR.onreadystatechange = function(){
+    if(this.readyState==4 && this.status == "200"){
+        var post_data = JSON.parse(postDataR.responseText);
+
+        var postCon = document.createElement("div");
+        var postImage = document.createElement("img");
+        var post_title = document.createElement("h1");
+        var post_writer = document.createElement('p');
+        var post_meta_data = document.createElement('div');
+        var data = document.createElement('div');
+
+        postCon.classList.add("post-con");
+        postImage.classList.add("post-image");
+        post_title.classList.add("post-title");
+        post_meta_data.classList.add('post-info');
+        post_writer.classList.add('meta-data');
+        data.classList.add('data');
+
+        post.appendChild(postCon);
+        postCon.appendChild(postImage);
+        postCon.appendChild(post_title);
+        postCon.appendChild(post_meta_data);
+        post_meta_data.appendChild(post_writer);
+        postCon.appendChild(data);
+
+        document.title = post_data[0].topic;
+        postImage.src = post_data[0].image;
+        post_title.innerHTML = post_data[0].topic;
+        post_writer.innerHTML = "<i>Category : </i>"+post_data[0].category +"<br><i>Contributed by : </i>"+"<img src="+post_data[1].writerimage+">"+post_data[0].writer;
+        data.innerHTML = "<p>"+post_data[0].content[0].para1+"<br><br>"+post_data[0].content[0].para2+"</p>";
+    }
 }
